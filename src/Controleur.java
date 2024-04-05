99311b35b2ea6576ce070b7f570c66d80a70a5f0
quarriz <quarriz@3il.fr>
2024-04-05 15:46:03 +0200

    Javadoc

--- src/Controleur.java
+++ src/Controleur.java
@@ -5,11 +5,16 @@ import java.awt.event.ActionEvent;
 
 public class Controleur implements ActionListener
 {
+	/*
+	 * Va gérer la communication entre la partie graphique et model
+	 */
 	public Controleur()
 	{
 		
 	}
-	
+	/*
+	 * Lorsque cette action event est activé, elle va creer le constructeur task de la partie modele grace au information des différents libelle
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e)
 	{
